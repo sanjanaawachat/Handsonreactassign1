209cdfdc99b81693306fbd9480a63595f10edc3b
sanjanaawachat <104431783+sanjanaawachat@users.noreply.github.com>
2022-08-03 18:07:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import './style.css'
+function App() {
+  return (
+      <div className="App">
+      <h1 className="text1">Styling Using Functional and Class Component</h1>
+      <button className="btn1">To see styling in functional component</button>
+      <button className="btn2">To see styling in class component</button>
+    </div>
+    
+  );
+}
+
+export default App;
